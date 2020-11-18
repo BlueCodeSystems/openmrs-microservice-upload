ee047e469950df84c12e4f6336c1e0d04e97d3df
Makili Musepa <makilimusepa@gmail.com>
2020-11-18 14:41:54 +0200

    change EDi image directory

--- controller/upload.js
+++ controller/upload.js
@@ -3,7 +3,7 @@ import fs from "fs";
 
 function uploadFiles(req, res) {
         
-    const EDI_DIR = process.env.ROOT_DIR || '/usr/src/openmrs-microservice-upload/EDI';
+    const EDI_DIR = process.env.EDI_DIR || '/usr/src/openmrs-microservice-upload/EDI';
     if (!req.files || Object.keys(req.files).length === 0) {
         return res.status(400);
     }
