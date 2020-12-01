e99ee05559a25f44fd8af4bdcec4c751bb25ba93
Makili Musepa <makilimusepa@gmail.com>
2020-12-01 16:03:25 +0200

    bypass cors error using cors-anywhere

--- controller/upload.js
+++ controller/upload.js
@@ -1,17 +1,16 @@
-import moment from "moment";
 import fs from "fs";
 
 function uploadFiles(req, res) {
         
     const EDI_DIR = process.env.EDI_DIR || '/usr/src/openmrs-microservice-upload/EDI';
     if (!req.files || Object.keys(req.files).length === 0) {
-        return res.status(400);
+        return res.status(400).send('No flies were uploaded.');
     }
 
     let name = Object.keys(req.files)[0]
     let files = req.files[name];
-
-    const dir = `${EDI_DIR}/${name}/${(new Date.valueOf()).toString()}`
+    
+    const dir = `${EDI_DIR}/${name}/${(Date.now()).toString()}`
 
     fs.mkdirSync(dir, { recursive: true });
 
